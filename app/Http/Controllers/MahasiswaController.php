b646eab85d2891cec390ecad845998622c49871e
bulfa <shbsetia@gmail.com>
2021-06-05 15:02:54 +0700

    first commit

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mahasiswa;
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $mahasiswa = Mahasiswa::all();
+        return view('mahasiswa0217', ['mahasiswa'=>$mahasiswa]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('mahasiswa_tambah');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Mahasiswa::create([
+            'nbi' => $request->nbi,
+            'nama_mhs' => $request->nama_mhs
+        ]);
+        return redirect ('mahasiswa');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $mahasiswa = Mahasiswa::find($id);
+        return view('mahasiswa_edit', ['mahasiswa => $mahasiswa']);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $mahasiswa = Mahasiswa::find($id);
+        $mahasiswa -> nbi = $request -> nbi;
+        $mahasiswa -> nama_mhs = $request -> nama_mhs;
+        $mahasiswa -> save();
+        
+        return redirect('mahasiswa');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
